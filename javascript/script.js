4e58777a29bf812a0967656886d824177e6d930d
Caleb <dougalcaleb@gmail.com>
2020-09-23 16:09:11 -0600

    Add groups

--- javascript/script.js
+++ javascript/script.js
@@ -1,8 +1,30 @@
 function noteEdits() {
     for(var a = 0; a < document.querySelectorAll(".group-edit").length; a++) {
         document.querySelectorAll(".group-edit")[a].addEventListener("click", function() {
-            document.querySelectorAll(".group-edit")[a].parentNode.firstElementChild.setAttribute("contenteditable", "true");
-            document.querySelectorAll(".group-edit")[a].parentNode.firstElementChild.focus();
+            this.parentNode.children[0].setAttribute("contenteditable", "true");
+            this.parentNode.children[0].focus();
+            
+            var range = document.createRange();
+            range.selectNodeContents(this.parentNode.children[0]);
+            var selection = window.getSelection();
+            selection.removeAllRanges();
+            selection.addRange(range);
+            
+            this.style.visibility = "hidden";
+
+            this.parentNode.children[0].addEventListener("keydown", function(event) {
+                if (event.keyCode === 13) {
+                    this.parentNode.children[0].blur();
+                }
+            });
+
+            this.parentNode.children[0].addEventListener("blur", function() {
+                if (this.innerHTML === "") {
+                    this.innerHTML = "Group Title";
+                }
+                this.setAttribute("contenteditable", "false");
+                this.parentNode.children[1].style.visibility = "visible";
+            });
         });
     }
 }
