0026c4e8e791f0b8ffdf91f5acc9dbb3f3ad9cf4
dougalcaleb <dougalcaleb@gmail.com>
2020-09-28 10:28:38 -0600

    Can add multiple notes to a group

--- javascript/script.js
+++ javascript/script.js
@@ -1,8 +1,12 @@
 var activeGroup = null;
+var activeNote = null;
 var nextFree = 0;
 var editing = false;
 
 document.querySelector(".new-general").addEventListener("click", newGroup);
+document.querySelector(".new-note").addEventListener("click", function() {
+    newNote(activeGroup);
+});
 document.addEventListener("keydown", keydn);
 
 function keydn(event) {
@@ -26,7 +30,7 @@ function newGroup() {
     var ng = document.createElement("DIV");
     ng.classList.add("g"+nextFree, "group", "group-active");
     document.querySelector(".sidebar").appendChild(ng);
-    ng.innerHTML = "<p contenteditable='false' spellcheck='false' class='group-title'>Group Title</p><div class='group-menu'><svg viewBox='0 0 24 24' class='group-menu-delete'><path fill='currentColor' d='M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M9,8H11V17H9V8M13,8H15V17H13V8Z' /></svg><svg viewBox='0 0 24 24' class='group-menu-edit'><path fill='currentColor' d='M20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18,2.9 17.35,2.9 16.96,3.29L15.12,5.12L18.87,8.87M3,17.25V21H6.75L17.81,9.93L14.06,6.18L3,17.25Z' /></svg><svg viewBox='0 0 24 24' class='group-menu-select'><path fill='currentColor' d='M22,16A2,2 0 0,1 20,18H8C6.89,18 6,17.1 6,16V4C6,2.89 6.89,2 8,2H20A2,2 0 0,1 22,4V16M16,20V22H4A2,2 0 0,1 2,20V7H4V20H16M13,14L20,7L18.59,5.59L13,11.17L9.91,8.09L8.5,9.5L13,14Z' /></svg></div><div class='group-notes'>    <div class='note n1'>New Note</div>    </div>";
+    ng.innerHTML = "<p contenteditable='false' spellcheck='false' class='group-title'>Group Title</p><div class='group-menu'><svg viewBox='0 0 24 24' class='group-menu-delete'><path fill='currentColor' d='M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M9,8H11V17H9V8M13,8H15V17H13V8Z' /></svg><svg viewBox='0 0 24 24' class='group-menu-edit'><path fill='currentColor' d='M20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18,2.9 17.35,2.9 16.96,3.29L15.12,5.12L18.87,8.87M3,17.25V21H6.75L17.81,9.93L14.06,6.18L3,17.25Z' /></svg><svg viewBox='0 0 24 24' class='group-menu-select'><path fill='currentColor' d='M22,16A2,2 0 0,1 20,18H8C6.89,18 6,17.1 6,16V4C6,2.89 6.89,2 8,2H20A2,2 0 0,1 22,4V16M16,20V22H4A2,2 0 0,1 2,20V7H4V20H16M13,14L20,7L18.59,5.59L13,11.17L9.91,8.09L8.5,9.5L13,14Z' /></svg></div><div class='group-notes'>    <div class='note note-active n0'>New Note</div>    </div>";
 
     if (activeGroup !== null) {
         document.querySelector(".g"+activeGroup).classList.remove("group-active");
@@ -38,6 +42,27 @@ function newGroup() {
     editing = true;
     var e = new CustomEvent("click", { "detail": "true2" }, false);
     document.querySelector(".g"+activeGroup).children[1].children[1].dispatchEvent(e);
+}
+
+function newNote(group) {
+    for (var a = 0; a < document.querySelector(".g"+group).children[2].children.length; a++) {
+        document.querySelector(".g"+group).children[2].children[a].classList.remove("note-active");
+    }
+    var nn = document.createElement("DIV");
+    var nextNote = document.querySelector(".g"+group).children[2].children.length;
+    nn.classList.add("note", "note-active", "n"+nextNote);
+    document.querySelector(".g"+group).children[2].appendChild(nn);
+    nn.innerHTML = "New Note";
+
+    activeNote = (nextNote/1);
+
+    console.log("Active note is "+activeNote);
+
+    noteEdits();
+
+    editing = true;
+    var e = new CustomEvent("dblclick");
+    document.querySelector(".n"+activeNote).dispatchEvent(e);
 }
 
 
